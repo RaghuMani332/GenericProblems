eaff76de964b008d557b80d6b7b5ff7c4dae0cd6
raghu <subamraghu11154@gmail.com>
2024-02-07 11:55:43 +0530

    uc3

--- GenericsProblem.cs
+++ GenericsProblem.cs
@@ -9,9 +9,10 @@ namespace Problems
 {
     class GenericsProblem
     {
-        public float Max(float a,float b,float c)
+        public String Max(String a,String  b,String c)
         {
-            return a>b&&a>c?a:b>c&&b>a?b:c;
+            return a.CompareTo(b) > 0 && a.CompareTo(c) > 0 ? a : b.CompareTo(c) > 0 && b.CompareTo(a) > 0 ? b : c;
+         //   return a>b&&a>c?a:b>c&&b>a?b:c;
 
         }
     }
