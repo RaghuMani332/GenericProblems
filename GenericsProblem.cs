322ecf2509c22402259645756bbacd0552c21a58
raghu <subamraghu11154@gmail.com>
2024-02-07 11:47:16 +0530

    uc1

---
+++ GenericsProblem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems
+{
+    class GenericsProblem
+    {
+        public int Max(int a,int b,int c)
+        {
+            return a>b&&a>c?a:b>c&&b>a?b:c;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using Problems;
+using System;
+
+namespace MainMeth
+{
+
+
+
+    class LogicalProgram 
+    {
+        public static void Main()
+        {
+           
+            GenericsProblem problem = new GenericsProblem();
+            Console.WriteLine(problem.Max(5, 4, 3));
+        }
+
+    }
+
+
+
+}
